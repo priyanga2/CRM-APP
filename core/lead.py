d5eb689116f092a81bae5a96f8915ceea1ae041c
priyanga Arunachalaboopathy <priyangaa2226@gmail.com>
2022-03-19 14:51:48 +0000

    Full Lead feature source code changes available.

---
+++ core/lead.py
@@ -0,0 +1,16 @@
+##
+# Lead feature functionalities.
+##
+
+
+# Create Lead.
+def create_lead():
+  print "Created Lead successfully."
+
+  # Update Lead.
+def update_lead():
+  print "Lead updated successfully."
+
+  # Delete Lead.
+def delete_lead():
+  print "Lead deleted successfully."
